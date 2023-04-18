fde45de0c2a493964800d4f37f9f1088b76edcbb
Veronika Nevařilová <xnevar00@stud.fit.vutbr.cz>
2023-04-18 15:31:29 +0200

    Add header to interpret.py

--- interpret.py
+++ interpret.py
@@ -1,3 +1,9 @@
+#           IPP projekt 2
+#
+# author:   Veronika Nevarilova (xnevar00)
+# date:     4/2022
+# file:     interpret.py
+
 from classInterpret import Interpret
 
 if __name__ == "__main__":
